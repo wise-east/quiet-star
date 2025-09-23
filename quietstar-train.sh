629c83da39a3abc11afc4a7a4f61ee83f6dc6f24
Justin Cho <jcho@isi.edu>
2025-09-23 15:27:32 -0700

    fix slurm script 2

--- quietstar-train.sh
+++ quietstar-train.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
-# SBATCH --job-name=quietstar-train
-# SBATCH --output=quietstar-train.out
-# SBATCH --time=12:00:00
-# SBATCH --partition=gpu
-# SBATCH --gres=gpu:a100:1
-# SBATCH --cpus-per-task=32
-# SBATCH --mem=128G
-# SBATCH --mail-type=ALL
-# SBATCH --mail-user=hjcho@usc.edu
-# SBATCH --constraint=a100-80gb
 
+#SBATCH --job-name=quietstar-train
+#SBATCH --output=quietstar-train.out
+#SBATCH --time=12:00:00
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:a100:1
+#SBATCH --constraint=a100-80gb
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=128G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=hjcho@usc.edu
+
+source ~/.bashrc
 module purge
 module load conda
 module load cuda cudnn 
